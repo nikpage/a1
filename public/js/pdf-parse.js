c9781681bda7591eb297750f47e54cdf0d8c6e9c
Nik Page <nbpage@gmail.com>
2025-05-01 10:58:59 +0200

    Update pdf-parse.js

--- public/js/pdf-parse.js
+++ public/js/pdf-parse.js
@@ -1,17 +1,31 @@
-import * as pdfjsLib from 'https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.mjs';
-pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js';
-
 export async function parsePDF(file) {
-  const arrayBuffer = await file.arrayBuffer();
-  const pdf = await pdfjsLib.getDocument({ data: arrayBuffer }).promise;
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
 
-  let fullText = '';
-  for (let i = 1; i <= pdf.numPages; i++) {
-    const page = await pdf.getPage(i);
-    const content = await page.getTextContent();
-    const strings = content.items.map(item => item.str).join(' ');
-    fullText += strings + '\n';
-  }
+    reader.onload = async function () {
+      const typedarray = new Uint8Array(this.result);
 
-  return fullText;
+      try {
+        const pdf = await pdfjsLib.getDocument({ data: typedarray }).promise;
+        let text = '';
+
+        for (let i = 1; i <= pdf.numPages; i++) {
+          const page = await pdf.getPage(i);
+          const content = await page.getTextContent();
+          const pageText = content.items.map(item => item.str).join(' ');
+          text += pageText + '\n';
+        }
+
+        resolve(text);
+      } catch (error) {
+        reject(error);
+      }
+    };
+
+    reader.onerror = function (error) {
+      reject(error);
+    };
+
+    reader.readAsArrayBuffer(file);
+  });
 }
