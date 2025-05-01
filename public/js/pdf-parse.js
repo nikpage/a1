d064494805c9e01f454425282f14fda74a01f6b9
Nik Page <nbpage@gmail.com>
2025-05-01 11:10:15 +0200

    Update pdf-parse.js

--- public/js/pdf-parse.js
+++ public/js/pdf-parse.js
@@ -2,24 +2,9 @@ export async function parsePDF(file) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
 
-    reader.onload = async function () {
-      const typedarray = new Uint8Array(this.result);
-
-      try {
-        const pdf = await pdfjsLib.getDocument({ data: typedarray }).promise;
-        let text = '';
-
-        for (let i = 1; i <= pdf.numPages; i++) {
-          const page = await pdf.getPage(i);
-          const content = await page.getTextContent();
-          const pageText = content.items.map(item => item.str).join(' ');
-          text += pageText + '\n';
-        }
-
-        resolve(text);
-      } catch (error) {
-        reject(error);
-      }
+    reader.onload = function () {
+      const text = new TextDecoder('utf-8').decode(new Uint8Array(this.result));
+      resolve(text);
     };
 
     reader.onerror = function (error) {
