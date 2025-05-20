8bf32bcd1c39d457ea32d586c03f702ef274f4ad
Nik Page <nbpage@gmail.com>
2025-05-20 10:03:24 +0200

    dbjs

--- test.js
+++
@@ -1,9 +0,0 @@
-import { supabase } from './supabaseClient.js';
-
-(async () => {
-  const { data, error } = await supabase
-    .from('cv_metadata')
-    .select('*')
-    .limit(1);
-  console.log({ data, error });
-})();
