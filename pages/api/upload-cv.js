20aa583eb722b802d6b661bf1341066b24a06e6d
Nik Page <nbpage@gmail.com>
2025-06-16 00:05:36 +0200

    <zzz

--- pages/api/upload-cv.js
+++ pages/api/upload-cv.js
@@ -1,4 +1,10 @@
 // pages/api/upload-cv.js
+console.log('ENV VARS CHECK:', {
+  SUPABASE_URL: process.env.SUPABASE_URL,
+  SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+  NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+})
 
 console.log("Handler HIT")
 
