b0beeca9c43fe8ea8fe8713fb4319bff2cf6f53b
Nik Page <nbpage@gmail.com>
2025-06-15 14:45:24 +0200

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -12,14 +12,11 @@ const supabase = createClient(
 )
 
 export async function upsertUser(user_id) {
-  const { data, error } = await supabase
-    .from('users')
-    .upsert({ user_id, tokens: 3 })
-    .select()
-
+  const { data, error } = await supabase.from('users').upsert({ user_id, tokens: 3 }).select()
   if (error) throw error
   return data[0]
 }
+
 
 export async function upsertCV(user_id, cv_file_url, cv_data) {
   const { error } = await supabase
