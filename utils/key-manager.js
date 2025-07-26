e72714615e7e4c68ca0df30622bc4bad3d806a66
Nik Page <nbpage@gmail.com>
2025-07-26 12:15:55 +0200

    Update key-manager.js

--- utils/key-manager.js
+++ utils/key-manager.js
@@ -37,7 +37,7 @@ export class KeyManager {
         }
     }
 
-    
+
 
     getNextKey() {
       if (this.keys.length === 0 || this.keys.every(k => k === null)) {
@@ -51,7 +51,7 @@ export class KeyManager {
       const envKeyName = Object.entries(process.env).find(([name, value]) => value === key)?.[0];
 
       console.log(`[KeyManager] Using key: ${envKeyName} = ${key.slice(0, 12)}...`);
-
+console.log(`[KeyManager] Using key ${envKeyName} ending in ****${key.slice(-4)}`);
       this.currentKeyIndex = (this.currentKeyIndex + 1) % this.keys.length;
       return key;
     }
